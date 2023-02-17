4090b116f894633a7b0af484c82c9426760c97e2
koba <kobagogoberidze21@gmail.com>
2023-02-18 01:00:47 +0400

    Declare container global variable and fit router

--- src/public/index.php
+++ src/public/index.php
@@ -3,6 +3,7 @@
 use App\App;
 use App\Router;
 use App\Config;
+use App\Container;
 use App\Controllers;
 
 require_once '../vendor/autoload.php';
@@ -16,7 +17,9 @@ define('VIEW_PATH', __DIR__ . '/../views');
 $config = new Config($_ENV);
 $request = array('uri' => $_SERVER['REQUEST_URI'], 'method' => $_SERVER['REQUEST_METHOD']);
 
-$router = new Router();
+$container = new Container();
+
+$router = new Router($container);
 $router->get('/', array(Controllers\HomeController::class, 'index'));
 
 $app = new App($router, $request, $config);
